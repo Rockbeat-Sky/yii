c431dc35f7117f64b327efe72f06132ec6e53349
hansen_wong <huanghanzen@gmail.com>
2016-07-21 01:09:31 +0800

    init

---
+++ src/db/ActiveRecord.php
@@ -0,0 +1,12 @@
+<?php
+namespace sky\yii\db;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class ActiveRecord extends \yii\db\ActiveRecord
+{
+    
+}
